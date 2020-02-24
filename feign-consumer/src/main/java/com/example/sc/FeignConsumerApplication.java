0f9e0821b1f23ca9dc7a58112373375f8bf52516
Aspirin018 <438238118@qq.com>
2020-02-25 01:31:36 +0800

    config feign log level.

--- feign-consumer/src/main/java/com/example/sc/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/example/sc/FeignConsumerApplication.java
@@ -1,12 +1,19 @@
 package com.example.sc;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @SpringBootApplication
 public class FeignConsumerApplication {
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FeignConsumerApplication.class, args);
