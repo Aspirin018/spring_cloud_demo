440c801622610128ec68d636cf7bff02567ba880
Aspirin018 <438238118@qq.com>
2020-02-25 01:59:19 +0800

    config service route by zuul.

--- feign-consumer/src/main/java/com/example/sc/refactor_conusmer/RefactorConsumerController.java
+++ feign-consumer/src/main/java/com/example/sc/refactor_conusmer/RefactorConsumerController.java
@@ -5,18 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RefactorConsumerController {
-
-    @Autowired
-    RefactorConsumerService refactorConsumerService;
-
-    @GetMapping(value="/feign-consumer-refactor")
-    public String auctionConsumerRefactor(){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(refactorConsumerService.auctionRefactor1("apple")).append("\n");
-        stringBuilder.append(refactorConsumerService.auctionRefactor2("peach", 20d)).append("\n");
-        stringBuilder.append(refactorConsumerService.auctionRefactor3(new Goods("peach", 20d))).append("\n");
-        return stringBuilder.toString();
-    }
-}
+//@RestController
+//public class RefactorConsumerController {
+//
+//    @Autowired
+//    RefactorConsumerService refactorConsumerService;
+//
+//    @GetMapping(value="/feign-consumer-refactor")
+//    public String auctionConsumerRefactor(){
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(refactorConsumerService.auctionRefactor1("apple")).append("\n");
+//        stringBuilder.append(refactorConsumerService.auctionRefactor2("peach", 20d)).append("\n");
+//        stringBuilder.append(refactorConsumerService.auctionRefactor3(new Goods("peach", 20d))).append("\n");
+//        return stringBuilder.toString();
+//    }
+//}
--- feign-consumer/src/main/java/com/example/sc/refactor_conusmer/RefactorConsumerService.java
+++ feign-consumer/src/main/java/com/example/sc/refactor_conusmer/RefactorConsumerService.java
@@ -3,6 +3,6 @@ package com.example.sc.refactor_conusmer;
 import com.example.sc.auction.AuctionService;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient(value = "AUCTION-SERVICE")
-public interface RefactorConsumerService extends AuctionService {
-}
+//@FeignClient(value = "AUCTION-SERVICE")
+//public interface RefactorConsumerService extends AuctionService {
+//}
